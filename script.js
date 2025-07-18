3f160666a53751d5e038820b1f42df3384144872
Takakio <takahashikio@gmail.com>
2025-07-18 12:16:19 -0300

    Implementado o calculo para descontos em produtos

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ function calcularPorcentagem(){
     let resultado = (valor/100)*porcentagem;
 
     document.getElementById("resultado").textContent = resultado;
+    document.getElementById("apresentarValor").textContent = valor;
+    document.getElementById("apresentarPorcentagem").textContent = porcentagem;
+    document.getElementById("apresentarResultado").style.display = "block";
+}
+
+function calcularDesconto(){
+    let valorProduto = document.getElementById("valorProduto").value;
+    let porcentagemDesconto = document.getElementById("porcentagemDesconto").value;
+    let valorDesconto = (valorProduto/100)*porcentagemDesconto;
+    let valorFinal = valorProduto-valorDesconto;
+
+    document.getElementById("apresentarValorProduto").textContent = valorProduto;
+    document.getElementById("apresentarDesconto").textContent = porcentagemDesconto;
+    document.getElementById("valorFinal").textContent = valorFinal;
+    document.getElementById("apresentarValorComDesconto").style.display = "block";
 }
 
